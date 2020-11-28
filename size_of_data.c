8bcbf5ae55564e06a24a77b3ca43d4b754a7875a
ellitenut <75153942+ellitenut@users.noreply.github.com>
2020-11-28 13:37:45 +0800

    Add files via upload

---
+++ size_of_data.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+ 
+int main(){
+	printf("the value of INT_MAX is %i\n",INT_MAX);
+	printf("the value of INT_MIN is %i\n",INT_MIN);
+	printf("hello world!"); 
+	
+	printf("the value of DBL_MAX is %f\n",DBL_MAX);
+	printf("the value of DBL_MIN is %f\n",DBL_MIN);
+	
+	return 0;
+} 
